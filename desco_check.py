ac7f7b5398ae2b6ac5945136923caa3df7fb10d1
Fazle Rabbi <fazlerabbicse65@gmail.com>
2026-06-21 23:37:16 +0600

    Update desco_check.py

--- desco_check.py
+++ desco_check.py
@@ -29,7 +29,7 @@ METER_NO = "066120003770"
 # 200 = alert below 200 BDT
 # 50  = alert below 50 BDT
 
-THRESHOLD = 350
+THRESHOLD = 400
 
 # =====================================================
 # EMAIL RECEIVERS
